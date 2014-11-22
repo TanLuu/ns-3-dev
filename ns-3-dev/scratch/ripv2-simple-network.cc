3a4c108a38a7c80333e4dc252592a7631d49c70f
luuhoangtan@gmail.com <luuhoangtan@gmail.com@dea4a21f-6f0d-48cd-6795-3b33840faf1a>
2014-11-22 03:13:28 +0000

    Added team member names.

--- ns-3-dev/scratch/ripv2-simple-network.cc
+++ ns-3-dev/scratch/ripv2-simple-network.cc
@@ -16,6 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Tommaso Pecorella <tommaso.pecorella@unifi.it>
+ * Edited by Tan Luu <luuhoangtan@gmail.com>
+ * and Thoan Pham <phamngocthoan2171991@gmail.com>
  */
 
 // Network topology
